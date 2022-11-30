6fc7f295f6b2a6670226748859d36064cc678ba4
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2022-11-30 19:26:30 +0530

    update portfolio

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # My Portfolio
+
+- `npm i react-typical`
+- `npm i react-icons`
+- `npm i react-github-calendar`
+- `npm install react-tooltip`
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -16,13 +16,20 @@ export const Contact = () => {
                 <h1>Contact Me</h1>
                 <div>
                    <h3><FaMobileAlt className="contactlogotop"/>&nbsp;Contact No.&nbsp; › &nbsp;<span>9993977820</span></h3>
-                   <h3><FaMailBulk className="contactlogotop"/>&nbsp;&nbsp;Email Id &nbsp; › &nbsp;<span>bansiyalokesh29988@gmail.com</span></h3>
+                   <h3>
+                      <a href="mailto:bansiyalokesh29988@gmail.com">
+                        <FaMailBulk className="contactlogotop"/>&nbsp;&nbsp;Email Id &nbsp; › &nbsp;<span>bansiyalokesh29988@gmail.com</span>
+                      </a>
+                   </h3>
                    <h3><FaHome className="contactlogotop"/>&nbsp;&nbsp; Address&nbsp; › <span>Badagaon, Nalkheda (Madhya Pradesh)</span></h3>
                 </div>
                 <div>
                     <a href="https://www.linkedin.com/in/lokesh-patidar-aa45a0236/" target="_blank"><FaLinkedinIn className="contactlogo"/></a>
                     <a href="https://github.com/lokesh-patidar" target="_blank"><FaGithub className="contactlogo"/></a>
                     <a href="mailto:bansiyalokesh29988@gmail.com"><FaMailBulk className="contactlogo"/></a>
+                </div>
+                <div>
+                    <p className="thank-you">Thank you for visiting my portfolio!</p>
                 </div>
             </div>
         </div>
