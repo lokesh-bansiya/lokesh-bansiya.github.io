1812483cbd6d905515d94f21f550432dc3290aed
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2023-03-08 12:24:48 +0530

    portfolio update

---
+++ src/Components/GitStar.jsx
@@ -0,0 +1,55 @@
+import { Image } from "@chakra-ui/react";
+import React from "react";
+import Slider from "react-slick";
+
+import img1 from "./Carousel_Images/gitStar1.png";
+import img2 from "./Carousel_Images/gitStar2.png";
+import img3 from "./Carousel_Images/gitStar3.png";
+import img4 from "./Carousel_Images/gitStar4.png";
+import img5 from "./Carousel_Images/gitStar5.png";
+import img6 from "./Carousel_Images/gitStar6.png";
+import img7 from "./Carousel_Images/gitStar7.png";
+import img8 from "./Carousel_Images/gitStar8.png";
+
+
+const GitStar = () => {
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 4000,
+        autoplaySpeed: 5,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+    };
+    return (
+        <Slider {...settings}>
+            <div>
+                <Image width="100%" height="100%" src={img1} alt="img1" />
+            </div>
+            <div>
+                <Image width="100%" height="100%" src={img2} alt="img2" />
+            </div>
+            <div>
+                <Image width="100%" height="100%" src={img3} alt="img3" />
+            </div>
+            <div>
+                <Image width="100%" height="100%" src={img4} alt="img4" />
+            </div>
+            <div>
+                <Image width="100%" height="100%" src={img5} alt="img5" />
+            </div>
+            <div>
+                <Image width="100%" height="100%" src={img6} alt="img1" />
+            </div>
+            <div>
+                <Image width="100%" height="100%" src={img7} alt="img2" />
+            </div>
+            <div>
+                <Image width="100%" height="100%" src={img8} alt="img3" />
+            </div>
+        </Slider>
+    );
+};
+
+export { GitStar };
